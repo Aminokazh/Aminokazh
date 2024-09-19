27f23f50b39187fd89cdf1a4912570a403bc7794
Aminokazh <aminokazh@gmail.com>
2024-09-19 05:32:23 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/"><i class="fas fa-home"></i> Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/register"><i class="fas fa-user-plus"></i> Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/login"><i class="fas fa-sign-in-alt"></i> Login</a>
+            </li>
+        </ul>
+    </nav>
+    
+    
+    <div class="main-container">
+        <h1 class="welcome-text">WELCOME</h1>
+        <p style="color: #1b4640;">To continue, please log in or register.</p>
+        <div class="main-buttons">
+            <a href="/login" class="btn main-btn">Login</a>
+            <a href="/register" class="btn main-btn">Register</a>
+        </div>
+    </div>
+    
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+<footer id="footer" class="footer-text">
+    <p>Created by girls from BDA-2304 &copy; 2024</p>
+</footer>
+
+
+</html>
